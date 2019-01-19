1158684d61e8df9272cb61f6d983a02132730d9e
z3by <zoaby.am@gmail.com>
2019-01-19 12:25:00 +0200

    UPDATE serialize all models

--- api/serializers.py
+++ api/serializers.py
@@ -13,3 +13,26 @@ class TerminalSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Terminal
         fields = ('id', )
+
+
+class MarchantSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Marchant
+        fields = ('id', 'email', 'phone')
+
+
+class ARQCSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ARQC
+        fields = ('ATC', 'TSI', 'TVR', 'AID', 'app_label')
+
+
+class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = (
+            'type', 'batch_no', 'trace_no', 'exp_date',
+            'ref_no', 'app_code', 'base',
+            'tips', 'signature_img', 'arqc',
+            'card', 'terminal', 'marchant'
+        )
