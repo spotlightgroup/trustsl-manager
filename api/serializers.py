33aee2dbf4c44afc6c00b67b04d4e5043a4af0eb
z3by <zoaby.am@gmail.com>
2019-01-19 13:07:07 +0200

    FEAT rest api serialized

--- api/serializers.py
+++ api/serializers.py
@@ -3,36 +3,39 @@ from rest_framework import serializers
 from api.models import Card, Terminal, Marchant, Transaction, ARQC
 
 
-class CardSerializer(serializers.HyperlinkedModelSerializer):
+class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ('no', 'exp_date')
 
 
-class TerminalSerializer(serializers.HyperlinkedModelSerializer):
+class TerminalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Terminal
         fields = ('id', )
 
 
-class MarchantSerializer(serializers.HyperlinkedModelSerializer):
+class MarchantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Marchant
         fields = ('id', 'email', 'phone')
 
 
-class ARQCSerializer(serializers.HyperlinkedModelSerializer):
+class ARQCSerializer(serializers.ModelSerializer):
     class Meta:
         model = ARQC
         fields = ('ATC', 'TSI', 'TVR', 'AID', 'app_label')
 
 
-class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+class TransactionSerializer(serializers.ModelSerializer):
+    marchant = MarchantSerializer()
+    terminal = TerminalSerializer()
+    arqc = ARQCSerializer()
+
     class Meta:
         model = Transaction
         fields = (
-            'type', 'batch_no', 'trace_no', 'exp_date',
+            'id', 'type', 'batch_no', 'trace_no', 'exp_date',
             'ref_no', 'app_code', 'base',
-            'tips', 'signature_img', 'arqc',
-            'card', 'terminal', 'marchant'
+            'tips', 'signature_img', 'marchant', 'terminal', 'arqc'
         )
--- api/viewSets.py
+++ api/viewSets.py
@@ -1,6 +1,6 @@
 from rest_framework import routers, viewsets
-from api.models import Card, Terminal
-from api.serializers import CardSerializer, TerminalSerializer
+from api.models import Card, Terminal, Marchant, Transaction, ARQC
+from api.serializers import CardSerializer, TerminalSerializer, MarchantSerializer, TransactionSerializer, ARQCSerializer
 
 
 class CardViewSet(viewsets.ModelViewSet):
@@ -13,7 +13,25 @@ class TerminalViewSet(viewsets.ModelViewSet):
     serializer_class = TerminalSerializer
 
 
+class MarchantViewSet(viewsets.ModelViewSet):
+    queryset = Marchant.objects.all()
+    serializer_class = MarchantSerializer
+
+
+class ARQCViewSet(viewsets.ModelViewSet):
+    queryset = ARQC.objects.all()
+    serializer_class = ARQCSerializer
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+
 router = routers.DefaultRouter()
 
 router.register('cards', CardViewSet)
 router.register('terminals', TerminalViewSet)
+router.register('marchants', MarchantViewSet)
+router.register('arqc', ARQCViewSet)
+router.register('transactions', TransactionViewSet)
