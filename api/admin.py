1ef390a9924d69d61a82bc3c1bc8d24d586227b2
z3by <zoaby.am@gmail.com>
2019-01-19 10:54:40 +0200

    register models in admin section

--- api/admin.py
+++ api/admin.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Product
+from .models import Marchant, Terminal, Transaction, ARQC, Card
 
-admin.site.register(Product)
+admin.site.register(Marchant)
+admin.site.register(Terminal)
+admin.site.register(Transaction)
+admin.site.register(ARQC)
+admin.site.register(Card)
