ff5ec43a67142cf7e1b0c67655bca953bf8c8346
z3by <zoaby.am@gmail.com>
2019-01-19 12:12:33 +0200

    FEAT rest terminals

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,9 @@ class CardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Card
         fields = ('no', 'exp_date')
+
+
+class TerminalSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Terminal
+        fields = ('id', )
--- api/viewSets.py
+++ api/viewSets.py
@@ -1,6 +1,6 @@
 from rest_framework import routers, viewsets
-from api.models import Card
-from api.serializers import CardSerializer
+from api.models import Card, Terminal
+from api.serializers import CardSerializer, TerminalSerializer
 
 
 class CardViewSet(viewsets.ModelViewSet):
@@ -8,6 +8,12 @@ class CardViewSet(viewsets.ModelViewSet):
     serializer_class = CardSerializer
 
 
+class TerminalViewSet(viewsets.ModelViewSet):
+    queryset = Terminal.objects.all()
+    serializer_class = TerminalSerializer
+
+
 router = routers.DefaultRouter()
 
 router.register('cards', CardViewSet)
+router.register('terminals', TerminalViewSet)
