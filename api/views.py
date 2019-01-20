05ca89408bc34da17dd458a9bc04b79a61a75641
z3by <zoaby.am@gmail.com>
2019-01-20 20:14:05 +0200

    FEAT  generate pdf from template

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from api.models import Transaction
 from django.template import loader
+from main.settings import BASE_DIR
 
 
 def receipt(request, id):
