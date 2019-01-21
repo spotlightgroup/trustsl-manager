44f0fa2bc149261aed165448ed93b79f75bfc140
z3by <zoaby.am@gmail.com>
2019-01-21 12:28:23 +0200

    FEAT cerate managment command for tcp server

--- main/settings.py
+++ main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
+    'tcp.apps.TcpConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
